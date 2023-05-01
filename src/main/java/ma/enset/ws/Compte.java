d19d5edc75ca661b45e6afe18856d6f69e710eef
Yassine Karimi <66490404+OchiHa@users.noreply.github.com>
2023-05-02 00:06:03 +0100

    Initial commit

--- src/main/java/ma/enset/ws/Compte.java
+++ src/main/java/ma/enset/ws/Compte.java
@@ -1,10 +1,18 @@
 package ma.enset.ws;
 
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
+
 import java.util.Date;
 
+@XmlRootElement(name = "compte")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Compte {
     private int code;
     private  double solde;
+    @XmlTransient
     private Date dateCreation;
 
     public Compte(int code, double solde, Date dateCreation) {
